90f3cbf72cc75a57555908a49de680520d6bdf6e
mizanmahi <mizanmahi24@gmail.com>
2021-07-20 15:26:41 +0600

    overriding and protected property

--- dist/app.js
+++ dist/app.js
@@ -1,23 +1,36 @@
 "use strict";
 class Department {
-    constructor(name, id) {
+    constructor(name, id, employees) {
         this.name = name;
         this.id = id;
+        this.employees = employees;
     }
     info() {
         console.log("Department: " + this.name + " and the id is: " + this.id);
+        console.log(this.employees);
+    }
+    addEmployee(employe) {
+        this.employees.push(employe);
     }
 }
 class ItDept extends Department {
     constructor(id, admin) {
-        super("IT", id);
+        super("IT", id, []);
         this.admin = admin;
     }
     printAdmin() {
         console.log(this.admin);
     }
+    addEmployee(employee) {
+        if (employee === "mizan")
+            return;
+        this.employees.push(employee);
+    }
 }
-const dept = new Department("CSE", 19320002);
+const dept = new Department("CSE", 19320002, ["Rafsan emp", "Rana emp"]);
 const It = new ItDept(19520005, ["Mizan", "kamal"]);
 console.log(dept);
 console.log(It);
+It.addEmployee("mizan");
+It.addEmployee("Milon");
+It.info();
--- src/app.ts
+++ src/app.ts
@@ -1,34 +1,41 @@
 class Department {
-   //    public name: string;
-   //    private id: number;
-   //    constructor(n: string, id: number) {
-   //       this.name = n;
-   //       this.id = id;
-   //    }
+   constructor(
+      public readonly name: string,
+      private id: number,
+      protected employees: string[]
+   ) {} // shrthand initialization
 
-   // this is the same as above initializaton of property, private and public modifier are mendatory here, this will automatically crate the properties named nmae and id
-   constructor(public readonly name: string, private id: number) {} // shrthand initialization
-
-   // here we can pass "this" as a parameter in typescipt, this will ensure that this method always refer to the instance of this particular class
    info(this: Department) {
       console.log("Department: " + this.name + " and the id is: " + this.id);
+      console.log(this.employees);
+   }
+
+   addEmployee(employe: string) {
+      this.employees.push(employe);
    }
 }
 
 class ItDept extends Department {
-   // admin is here automatically gets initialized and it is a property of this specific class
    constructor(id: number, public admin: string[]) {
-      super("IT", id); // passing the id to the parent class or base class
+      super("IT", id, []);
    }
-
    printAdmin() {
       console.log(this.admin);
    }
+
+   addEmployee(employee: string){ // overriding trhe method of base class
+       if(employee === "mizan") return;
+       this.employees.push(employee); // this works bcz employees array are now protected not private
+   }
 }
 
-const dept = new Department("CSE", 19320002);
-
+const dept = new Department("CSE", 19320002, ["Rafsan emp", "Rana emp"]);
 const It = new ItDept(19520005, ["Mizan", "kamal"]);
 
 console.log(dept);
 console.log(It);
+
+It.addEmployee("mizan");
+It.addEmployee("Milon");
+
+It.info()
