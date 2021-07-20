4968bc041f7868f22e90027e396165bac1761583
mizanmahi <mizanmahi24@gmail.com>
2021-07-20 19:41:17 +0600

    abstract classes and methods

--- dist/app.js
+++ dist/app.js
@@ -5,43 +5,15 @@ class Department {
         this.id = id;
         this.employees = employees;
     }
-    info() {
-        console.log("Department: " + this.name + " and the id is: " + this.id);
-        console.log(this.employees);
-    }
-    get printId() {
-        return this.id;
-    }
-    set changeId(newId) {
-        if (newId > 100)
-            throw new Error("Id must be less than 100");
-        this.id = newId;
-    }
-    addEmployee(employe) {
-        this.employees.push(employe);
-    }
 }
 class ItDept extends Department {
     constructor(id, admin) {
         super("IT", id, []);
         this.admin = admin;
     }
-    printAdmin() {
-        console.log(this.admin);
-    }
-    addEmployee(employee) {
-        if (employee === "mizan")
-            return;
-        this.employees.push(employee);
+    describe() {
+        console.log("Department name is: " + this.name);
     }
 }
-const dept = new Department("CSE", 19320002, ["Rafsan emp", "Rana emp"]);
-const It = new ItDept(19520005, ["Mizan", "kamal"]);
-console.log(dept);
-console.log(It);
-It.addEmployee("mizan");
-It.addEmployee("Milon");
-It.info();
-console.log(It.printId);
-It.changeId = 25;
-console.log(It.printId);
+const it = new ItDept(2365, []);
+it.describe();
--- src/app.ts
+++ src/app.ts
@@ -1,57 +1,27 @@
-class Department {
+abstract class Department {
    constructor(
       public readonly name: string,
       private id: number,
       protected employees: string[]
    ) {} // shrthand initialization
 
-   info(this: Department) {
-      console.log("Department: " + this.name + " and the id is: " + this.id);
-      console.log(this.employees);
-   }
-
-   get printId(){ // getter 
-       return this.id
-   }
-
-   set changeId(newId: number){ //setter
-       if(newId > 100) throw new Error("Id must be less than 100");
-       this.id = newId;
-   }
-
-   addEmployee(employe: string) {
-      this.employees.push(employe);
-   }
+   abstract describe(): void;
+  
 }
 
 class ItDept extends Department {
    constructor(id: number, public admin: string[]) {
       super("IT", id, []);
    }
-   printAdmin() {
-      console.log(this.admin);
-   }
 
-   addEmployee(employee: string){ // overriding trhe method of base class
-       if(employee === "mizan") return;
-       this.employees.push(employee); // this works bcz employees array are now protected not private
+   // all class extended by the Department must implement this method
+   describe(){
+      console.log("Department name is: " + this.name);
    }
 }
 
-const dept = new Department("CSE", 19320002, ["Rafsan emp", "Rana emp"]);
-const It = new ItDept(19520005, ["Mizan", "kamal"]);
+//const dept = new Department("Cse", 2569, []); //Cannot create an instance of an abstract class
 
-console.log(dept);
-console.log(It);
-
-It.addEmployee("mizan");
-It.addEmployee("Milon");
-
-It.info()
-console.log(It.printId);
-
-It.changeId =25;
-
-console.log(It.printId);
-//console.log(It.id); // id is private so can't be accessed like this
+const it = new ItDept(2365, [])
+it.describe()
 
