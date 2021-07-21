c189428fcdef8b06517802132af1c0e05837b26c
mizanmahi <mizanmahi24@gmail.com>
2021-07-22 01:15:13 +0600

    inersection type and type guard

--- dist/app.js
+++ dist/app.js
@@ -1,29 +1,7 @@
 "use strict";
-let cow;
-cow = {
-    name: "Bagha",
-    color: "red",
-    eatable: true,
-    age: 5,
-    info: () => { },
-};
-class Greet {
-    constructor(n) {
-        this.name = n;
-    }
-    greet() {
-        console.log("This is implemented on greetable interfaces");
-    }
-}
-const greet = new Greet("Hello there!");
-greet.greet();
-class Book {
-    constructor(book, pages) {
-        this.book = book;
-        this.pages = pages;
-    }
-}
-let addFn;
-addFn = (x, y) => {
-    return x + y;
+let obj;
+obj = {
+    name: "mzian",
+    privilege: ["good"],
+    startDate: new Date(),
 };
--- src/app.ts
+++ src/app.ts
@@ -1,80 +1,25 @@
-// interfaces
-// usually describe how an onject should look like
+// intersection types allow us to combine other types
 
-// declaration
-interface Animal {
-   // this could be used as a structure of an aobject
+type Admin = {
    name: string;
-   color: string;
-   eatable: boolean;
-   age: number;
-   info(): void;
-}
-
-let cow: Animal;
-
-cow = {
-   name: "Bagha",
-   color: "red",
-   eatable: true,
-   age: 5,
-   info: () => {},
+   privilege: string[];
 };
 
-// ----- with classes -------
-
-interface Greetable {
+type Employee = {
    name: string;
-   greet(): void;
-}
-
-class Greet implements Greetable {
-   // Greet must implement the Greeadable interface
-   name: string;
-
-   constructor(n: string) {
-      this.name = n;
-   }
-
-   greet() {
-      console.log("This is implemented on greetable interfaces");
-   }
-}
-
-const greet = new Greet("Hello there!");
-
-greet.greet();
-
-// ------ readonly property, extending interfaces  ---------
-
-interface other {
-   book: string;
-}
-
-interface book extends other {
-   readonly pages: number; // interfaces can have readonly property
-}
-
-class Book implements book {
-   constructor(public book: string, readonly pages: number) {} // page should have as read only property
-}
-
-// ----------- ----------- interface as a type of function -----------
-
-interface addfn {
-   // exception: this could be uses as a type of function. because in the end of the day function is also an object
-   (a: number, b: number): number;
-}
-
-let addFn: addfn;
-
-addFn = (x: number, y: number) => {
-   return x + y;
+   startDate: Date;
 };
 
+// this intersection can be done with the interface too by extends keyword
+type intersectAdminAndEmployee = Admin & Employee;
 
-//--------- optional parameter and properties ----------------------
-interface others {
-   animal?: string; // optional
-   people: string;
-}
+let obj: intersectAdminAndEmployee;
+
+// this object must have to have name, privilege, and startDate properties
+obj = {
+   name: "mzian",
+   privilege: ["good"],
+   startDate: new Date(),
+};
+
+// type guard
